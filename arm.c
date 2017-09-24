836e38c6c310acdeb5ebb88db3ad71cc815c79c5
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2017-09-24 15:59:33 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,t,rem,result=0;
+printf("enter the number");
+scanf("%d",&n);
+t=n;
+while(n!=0)
+{
+rem=n%10;
+}
+if(t==result)
+{
+printf("%d is an armstrong number");
+}
+else
+{
+printf("%d is not an armstrong number"):
+}
+return 0;
+}

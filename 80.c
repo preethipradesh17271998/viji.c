f354d23746fbef2e1c0552940dc48fc5c4538889
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-26 15:46:23 +0530

    Update 80.c

--- 80.c
+++ 80.c
@@ -5,7 +5,7 @@ void main()
     printf("enter the value ");
     scanf("%d",&n);
     printf("enter the numbers ");
-    for(i==0;i<n;i++)
+    for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }

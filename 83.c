7ab03c0223cf668a1351f1db8f0e65ab0424d7e8
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-03-03 19:14:34 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,c,d;
+        printf("enter the value");
+        scanf("%d%d",&a,&b);
+        printf("enter the value");
+        scanf("%d%d",&c,&d);
+        printf("the modulo is %d",(c%d));
+        if(a>b)
+        {
+        printf("the division is %d",(a/b));
+        }
+        else
+        {
+        	printf("the division is %d",(b/a));
+        }
+        
+            
+        }

0ca910000848bc4c5c2e62b8363338deb99fce1f
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-03-04 17:01:46 +0530

    Update sn.c

--- sn.c
+++ sn.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
-#include<conio.h>
-int main()
+void main()
 {
-int n,i,sum=0;
-printf("enter the number");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-sum=sum+i;
-}
-printf("sum of n numbers is %d",sum");
-return 0;
+    int n,k,a[10],i,sum=0;
+    printf("enter the number ");
+    scanf("%d%d",&n,&k);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=k;i++)
+    {
+        
+        sum=sum+i;
+    }
+    printf("%d",sum);
 }

e1e5332950842d5c98ad551689a7c0b2c262a550
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-03-04 16:51:11 +0530

    Update al.c

--- al.c
+++ al.c
@@ -5,7 +5,7 @@ void main()
 char ch;
 printf("enter any alphabet");
 scanf("%c",&ch);
-if(ch>=65&&ch<=90!!ch>=97&&ch<=122)
+if(ch>=65&&ch<=90||ch>=97&&ch<=122)
 {
 printf("entered character is alphabet");
 }
